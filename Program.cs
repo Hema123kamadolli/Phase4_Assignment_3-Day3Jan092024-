0aaeaecf53e4b6dff3f5b572cff12182c592f6e7
Hema123kamadolli <hema.kamadolli@mphasis.com>
2024-01-09 15:17:54 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, .Net Core Console App!");
+Console.WriteLine("Welcome is Jenkins Project world");
+Console.WriteLine("Welcome is Jenkins Project Build Example");
